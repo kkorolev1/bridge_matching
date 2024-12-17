6160b0d136360c93f0a09594c5407e71788679e4
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2024-12-17 13:14:28 +0300

    add warmup

---
+++ bridge_matching/utils/warmup_scheduler.py
@@ -0,0 +1,36 @@
+from torch.optim.lr_scheduler import _LRScheduler
+import math
+
+class WarmUpLRWithDecay(_LRScheduler):
+    def __init__(self, 
+                 optimizer,
+                 warmup_steps, 
+                 max_lr, 
+                 total_steps, 
+                 decay_strategy="cosine", 
+                 last_epoch=-1):
+    
+        self.warmup_steps = warmup_steps
+        self.max_lr = max_lr
+        self.total_steps = total_steps
+        self.decay_strategy = decay_strategy.lower()
+        assert decay_strategy in ["cosine", "exponential"]
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        if self.last_epoch < self.warmup_steps:
+            warmup_factor = (self.last_epoch + 1) / self.warmup_steps
+            return [base_lr + warmup_factor * (self.max_lr - base_lr) for base_lr in self.base_lrs]
+        else:
+            decay_step = self.last_epoch - self.warmup_steps
+            decay_steps = self.total_steps - self.warmup_steps
+
+            if self.decay_strategy == "cosine":
+                cosine_decay = 0.5 * (1 + math.cos(math.pi * decay_step / decay_steps))
+                return [self.max_lr * cosine_decay for _ in self.base_lrs]
+            elif self.decay_strategy == "exponential":
+                decay_rate = 0.1
+                exponential_decay = self.max_lr * (decay_rate ** (decay_step / decay_steps))
+                return [exponential_decay for _ in self.base_lrs]
+            
+                
