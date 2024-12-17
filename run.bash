28c58811faa58bfc796bab9433bae4a2e9b42b28
Kirill Korolev <kakorolev@edu.hse.ru>
2024-12-17 20:17:42 +0300

    Rename config

--- run.bash
+++
@@ -1,3 +0,0 @@
-export WANDB_API_KEY="24fb79a2b575b1f5b45d3e9f6955597fc65b9477" 
-PYTHONPATH=".:./edm" 
-accelerate launch --config_file accelerate_config.yaml --num_processes 1 train.py --config-name ffhq_inpainting.yaml 
