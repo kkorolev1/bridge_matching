ab42fcd00278da462457971056336b4cc27d3233
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2024-12-20 15:30:16 +0300

    add eval script

---
+++ eval_fid.py
@@ -0,0 +1,90 @@
+import warnings
+import sys
+from tqdm.auto import trange
+import os
+import numpy as np
+import torch
+from pathlib import Path
+import pickle
+import shutil
+import hydra
+from tqdm import tqdm
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
+from bridge_matching.dataset.dataloader import get_dataloaders
+from bridge_matching.utils import tensor_to_image
+from bridge_matching.metric.fid import calculate_inception_stats
+from bridge_matching.sampler import sample_euler
+warnings.filterwarnings("ignore", category=UserWarning)
+import dill
+# fix random seeds for reproducibility
+SEED = 42
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+
+def load_st_dct(path):
+    payload = torch.load(Path(path).open("rb"), pickle_module=dill)
+    state_dict = payload["state_dicts"]["model"]
+    new_state_dict = {}
+    for key in state_dict.keys():
+        if key.startswith("module"):
+            new_state_dict[key[7:]] = state_dict[key]
+    state_dict = new_state_dict
+    return state_dict
+
+
+@hydra.main(version_base=None, config_path="bridge_matching/config")
+@torch.inference_mode
+def main(config: DictConfig):
+    OmegaConf.resolve(config)
+    path_to_model = Path(config.path_to_model)
+    st_dct = load_st_dct(path_to_model)
+    model = instantiate(config.model)
+    model.load_state_dict(st_dct)
+    
+    bridge = instantiate(config.bridge)
+    transform = instantiate(config.transform)
+    sampling_params = config.sampling_params
+    fid_metric = instantiate(config.metrics[0])
+
+    images_dir = Path(config.images_dir)
+    dataloader = get_dataloaders(config.data)["test"]
+
+    model.eval()
+    image_index = 1
+    if os.path.exists(images_dir):
+        shutil.rmtree(images_dir)
+    os.makedirs(images_dir, exist_ok=True)
+    
+    with torch.no_grad():
+        for batch_idx, batch in tqdm(
+            enumerate(dataloader),
+            desc="Calc FID",
+            total=len(dataloader),
+        ):
+            x_orig = batch
+            x_trans = transform(x_orig)
+            x_pred, _ = sample_euler(
+                bridge,
+                model,
+                x_trans,
+                sampling_params,
+                save_history=False,
+            )
+            for i in range(x_pred.shape[0]):
+                image = tensor_to_image(x_pred[i])
+                image.save(images_dir / f"{image_index}.png")
+                image_index += 1
+        fid = fid_metric(images_dir)
+        print(f"FID: {fid}")
+    
+        
+        if os.path.exists(images_dir):
+            shutil.rmtree(images_dir)
+
+
+if __name__ == "__main__":
+    sys.argv.append("hydra.job.chdir=False")
+    main()
